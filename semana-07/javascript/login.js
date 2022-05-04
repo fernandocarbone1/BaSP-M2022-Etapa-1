280378277987bd57f4984919a736bd08e8dae88a
Fernando <fernando.carbone707@gmail.com>
2022-05-03 22:09:03 -0300

    BaSP-W07-2022: Modal from LogIn page

--- semana-07/javascript/login.js
+++ semana-07/javascript/login.js
@@ -77,7 +77,11 @@ var passwordPrint = document.getElementById('password');
 function confirmSubmit(){
     alert(
         "Your email is: " + emailPrint.value + 
-        "\nYour password is: " + password.value);  
+        "\nYour password is: " + password.value);
+    var modal = document.getElementById('modal').classList.remove('modal-content')
+    document.getElementById('mailModal').innerText = 'Your e-mail is: ' +  emailPrint.value
+    document.getElementById('passwordModal').innerText = 'Your password is: ' +  passwordPrint.value
+
 }
 
 function confirm() {
